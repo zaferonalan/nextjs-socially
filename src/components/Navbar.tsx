5b3ac316b11d079175d06ce5016a1981c218bae0
zafergunayonalan <zafergunay7@gmail.com>
2025-07-02 17:32:44 +0300

    userAction-created

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,14 @@ import Link from 'next/link'
 import React from 'react'
 import DesktopNavbar from './DesktopNavbar'
 import MobileNavbar from './MobileNavbar'
+import { currentUser } from '@clerk/nextjs/server'
+import { syncUser } from '@/actions/user.actions'
 
-const Navbar = () => {
+const Navbar = async() => {
+    const user = await currentUser()
+    if (user) {
+        await syncUser()
+    }
   return (
     <div className='sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50'>
         <div className='max-w-7xl mx-auto px-4'>
